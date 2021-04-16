d0f9a92d8f23ab4bb73c49a579bf9c2668f2e36f
Michael Brooks <michaelbrooks@google.com>
2021-04-16 10:37:10 -0700

    Add training script, update README
    Change-Id: I0775af514395626b8870507d935515be6865c94b

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# Manufacturing Demo
+# Coral Manufacturing Demo
+
+The Coral Manufacturing Demo demonstrates how to use a Coral Edge TPU to accomplish two common manufacturing use-cases:
+
+* Worker Safety
+* Visual Inspection
+
+With a single Coral Edge TPU, it is possible to run both applications in real time! The demo includes example videos designed for the specific application, but camera sources and other videos are accepted. The demo requires OpenGL and is intended for either x86 platforms or the [Coral Dev Board](https://coral.ai/products/dev-board/) but building for x86, ARM64, and ARMv7 is all supported.
+
+This demo is able to achieve realtime processing and display by taking advantage of the following features:
+
+* [Model Cocompilation](https://coral.ai/products/dev-board/) - The two models are compiled together, allowing the TPU cache to store both model parameters. This lets you switch between models without needing to reload.
+* Cascaded Models - For Visual Inspection, the output bounding box of the MobileDet detection is used to crop an image for the quality classifier. While in this demo only one apple is on a screen at a time, this approach can allow visual inspection of multiple objects in a frame.
+* Retraining (via Keras API) - The apple quality classifier shows how to use the Keras API to retrain a known model. More info can be found in the [retraining script](models/retraining/train_classifier.py).
+* OpenGL Acceleration - In order to mix the two demos, OpenGL is used. Also included is a new GStreamer element, glsvgoverlaysink, that allows generic systems to add a SVG overlay to an existing GL surface. This improves latency by not requiring a conversion to CPU for running [rsvgoverlay](https://gstreamer.freedesktop.org/documentation/rsvg/rsvgoverlay.html).
+
+## Demo Overview
+
+**Worker Safety**
+
+For worker safety, a keepout region is defined (from a [CSV file](config/keepout_points.csv)) and MobileDet (trained on COCO17 dataset) is run to detect people. When a person is outside of this region the bounding box is green, if they are within it is red. The algorithm for determining collisions can be found in [keepout_shape.cc](src/keepout_shape.cc).
+
+The default video is taken from [this repo](https://github.com/intel-iot-devkit/sample-videos).
+
+**Visual Inspection**
+
+The visual inspection demo grades the quality of apples on a conveyor belt. This is accomplished by cascading two models - MobileDet detection (for detecting an apple) and then cropping to the apple and running MobileNet v2 classification to determine if the apple is good or rotten. The detection model is the standard COCO17 detection available on the [Coral Model Page](https://coral.ai/models/), but the classification model required retraining. 
+
+For classification, this [fresh/rotten fruit dataset](https://www.kaggle.com/sriramr/fruits-fresh-and-rotten-for-classification) was used (taking only the apples). Keras API is used to load the base MobileNet V2 model and retain the [model](models/retraining/classifier.h5). TFlite APIs are then used for post-training quantization and conversion to [CPU TFLite](modles/retraining/classifier.tflite). Finally, the model is run through the EdgeTPU compiler with the MobileDet model to cocompile. A script for running this process on your own can be found [here](models/retraining/train_classifier.py).
 
 ## Compiling the demo
 
@@ -18,21 +46,11 @@ make DOCKER_TARGETS=demo DOCKER_CPUS=aarch64 docker-build
 
 The binary should be in `out/$ARCH/demo` directory.
 
-## Quick run
+## Run the Demo
 
-First, download example models using the provided script
-
-```
-sh download_models.sh
-```
-
-Run without collision detection:
+The default options will run the demo with the two example videos, a default keepout region, and the two cocompiled models (MobileDet and MobileNet V2).
 
 ```
 ./out/$ARCH/demo/manufacturing_demo
 ```
 
-Run with an example collision detection config:
-```
-./out/k8/demo/manufacturing_demo --keepout_points_path config/keepout_points.csv
-```
---
+++ models/retraining/train_classifier.py
@@ -0,0 +1,237 @@
+from tflite_runtime.interpreter import load_delegate
+from tflite_runtime.interpreter import Interpreter
+
+import glob
+import os
+import subprocess
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+input_size = (224, 224)
+input_shape = (224, 224, 3)
+batch_size = 1
+
+###########################################################################################
+# Load pretrained model
+###########################################################################################
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=input_shape,
+                                                    include_top=False,
+                                                    classifier_activation='softmax',
+                                                    weights='imagenet')
+# Freeze first 100 layers
+base_model.trainable = True
+for layer in base_model.layers[:100]:
+  layer.trainable = False
+
+model = tf.keras.Sequential([
+    base_model,
+    tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(units=2, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=tf.keras.optimizers.RMSprop(lr=1e-5),
+              metrics=['accuracy'])
+print(model.summary())
+
+###########################################################################################
+# Prepare Datasets
+###########################################################################################
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   zoom_range=0.3,
+                                   rotation_range=50,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+dataset_path = './dataset'
+train_set_path = os.path.join(dataset_path, 'train')
+val_set_path = os.path.join(dataset_path, 'test')
+batch_size = 64
+
+train_generator = train_datagen.flow_from_directory(train_set_path,
+                                                    target_size=input_size,
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical')
+val_generator = val_datagen.flow_from_directory(val_set_path,
+                                                target_size=input_size,
+                                                batch_size=batch_size,
+                                                class_mode='categorical')
+
+epochs = 15
+history = model.fit(train_generator,
+                    steps_per_epoch=train_generator.n // batch_size,
+                    epochs=epochs,
+                    validation_data=val_generator,
+                    validation_steps=val_generator.n // batch_size,
+                    verbose=1)
+
+###########################################################################################
+# Plotting Train Data
+###########################################################################################
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()), 1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0, 1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+# plt.show()
+plt.savefig('history.png')
+
+
+###########################################################################################
+# Post Training Quantization
+###########################################################################################
+
+def representative_data_gen():
+  dataset_list = tf.data.Dataset.list_files('./dataset/test/*/*')
+  for i in range(100):
+    image = next(iter(dataset_list))
+    image = tf.io.read_file(image)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, input_size)
+    image = tf.cast(image / 255., tf.float32)
+    image = tf.expand_dims(image, 0)
+    yield [image]
+
+
+model.input.set_shape((1,) + model.input.shape[1:])
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_data_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.target_spec.supported_types = [tf.int8]
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+tflite_model = converter.convert()
+
+###########################################################################################
+# Saving models
+###########################################################################################
+
+model.save('classifier.h5')
+with open('classifier.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+###########################################################################################
+# Evaluating h5
+###########################################################################################
+
+batch_images, batch_labels = next(val_generator)
+
+labels = '\n'.join(sorted(train_generator.class_indices.keys()))
+with open('classifier_labels.txt', 'w') as f:
+  f.write(labels)
+
+logits = model(batch_images)
+prediction = np.argmax(logits, axis=1)
+truth = np.argmax(batch_labels, axis=1)
+keras_accuracy = tf.keras.metrics.Accuracy()
+keras_accuracy(prediction, truth)
+
+###########################################################################################
+# Evaluating tflite
+###########################################################################################
+
+
+def set_input_tensor(interpreter, input):
+  input_details = interpreter.get_input_details()[0]
+  tensor_index = input_details['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  scale, zero_point = input_details['quantization']
+  input_tensor[:, :] = np.uint8(input / scale + zero_point)
+
+
+def classify_image(interpreter, input):
+  set_input_tensor(interpreter, input)
+  interpreter.invoke()
+  output_details = interpreter.get_output_details()[0]
+  output = interpreter.get_tensor(output_details['index'])
+  scale, zero_point = output_details['quantization']
+  output = scale * (output - zero_point)
+  top_1 = np.argmax(output)
+  return top_1
+
+
+interpreter = tf.lite.Interpreter('classifier.tflite')
+interpreter.allocate_tensors()
+
+# Collect all inference predictions in a list
+batch_prediction = []
+batch_truth = np.argmax(batch_labels, axis=1)
+
+for i in range(len(batch_images)):
+  prediction = classify_image(interpreter, batch_images[i])
+  batch_prediction.append(prediction)
+
+# Compare all predictions to the ground truth
+tflite_accuracy = tf.keras.metrics.Accuracy()
+tflite_accuracy(batch_prediction, batch_truth)
+
+###########################################################################################
+# Compiles model
+###########################################################################################
+
+subprocess.call(["edgetpu_compiler",
+                 "--show_operations",
+                 "classifier.tflite"])
+
+###########################################################################################
+# Evaluating tflite
+###########################################################################################
+
+
+interpreter = Interpreter('classifier_edgetpu.tflite', experimental_delegates=[
+                          load_delegate('libedgetpu.so.1.0')])
+interpreter.allocate_tensors()
+
+# Collect all inference predictions in a list
+batch_prediction = []
+batch_truth = np.argmax(batch_labels, axis=1)
+
+for i in range(len(batch_images)):
+  prediction = classify_image(interpreter, batch_images[i])
+  batch_prediction.append(prediction)
+
+# Compare all predictions to the ground truth
+edgetpu_tflite_accuracy = tf.keras.metrics.Accuracy()
+edgetpu_tflite_accuracy(batch_prediction, batch_truth)
+
+###########################################################################################
+# Show Results
+###########################################################################################
+
+print("Raw model accuracy: {:.2%}".format(keras_accuracy.result()))
+print("Quant TF Lite accuracy: {:.2%}".format(tflite_accuracy.result()))
+print("EdgeTpu Quant TF Lite accuracy: {:.2%}".format(
+    edgetpu_tflite_accuracy.result()))
